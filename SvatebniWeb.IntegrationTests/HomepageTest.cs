6e3d3d56e19a03a300598a02e3518516fe8aa0f2
Michal Trnka <MiTrnka@gmail.com>
2025-09-19 06:04:33 +0200

    Počáteční commit po resetu historie

---
+++ SvatebniWeb.IntegrationTests/HomepageTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using SvatebniWeb.Web;
+
+
+namespace SvatebniWeb.IntegrationTests;
+
+public class HomepageTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public HomepageTest(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Get_Homepage_ShouldReturn_SuccessStatusCode()
+    {
+        // Vytvori klienta, ktery bude posilat pozadavky na nasi aplikaci v pameti
+        var client = _factory.CreateClient();
+
+        // Posle pozadavek na hlavni stranku ("/")
+        var response = await client.GetAsync("/");
+
+        // Overi, ze odpoved byla uspesna (napr. status kód 200 OK)
+        response.EnsureSuccessStatusCode();
+    }
+}
---
+++ SvatebniWeb.UnitTests/PipelineTest.cs
@@ -0,0 +1,12 @@
+﻿namespace SvatebniWeb.UnitTests;
+
+public class PipelineTest
+{
+    [Fact]
+    public void SimpleUnitTest_ShouldAlways_Pass()
+    {
+        // Tento test jen overi, ze true je true.
+        // Slouzi ciste pro overeni, ze se testy v pipeline spusti.
+        Assert.True(true);
+    }
+}
